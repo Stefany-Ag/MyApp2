83d030ed11370791eb3191ecb28cb999e4099793
Fany <fatinker1703@hotmail.com>
2020-04-24 15:24:38 -0500

    Second commit

--- app/src/main/java/com/example/myapp2/Adapter.java
+++ app/src/main/java/com/example/myapp2/Adapter.java
@@ -1,25 +1,30 @@
 package com.example.myapp2;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Adapter extends BaseAdapter {
 
     private static LayoutInflater inflater = null;
     Context contexto;
     String[][] datos;
+    int[] imagen;
 
-    public Adapter(Context contexto, String[][] datos){
+    public Adapter(Context contexto, String[][] datos, int[] imagenes){
         this.contexto = contexto;
         this.datos = datos;
+        this.imagen = imagenes;
         inflater = (LayoutInflater) contexto.getSystemService(contexto.LAYOUT_INFLATER_SERVICE);
     }
     @Override
     public int getCount() {
-        return datos.length;
+        return imagen.length;
     }
 
     @Override
@@ -34,6 +39,16 @@ public class Adapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        final View vista = inflater.inflate(R.layout.element_list,null);
+        TextView nombre = vista.findViewById(R.id.tvNombre);
+        TextView categoria = vista.findViewById(R.id.tvCategoria);
+        TextView duracion = vista.findViewById(R.id.tvDuracion);
+        ImageView image = vista.findViewById(R.id.ivImagen);
+        nombre.setText(datos[position][0]);
+        categoria.setText(datos[position][1]);
+        duracion.setText(datos[position][2]);
+        image.setImageResource(imagen[position]);
+        image.setTag(position);
         return null;
     }
 }
